af83955aef7f79af6f56636205dd61fff9e8e6ce
Bung <crc32@qq.com>
2024-03-10 22:42:24 +0800

    complete test

--- tests/test1.nim
+++ tests/test1.nim
@@ -1,8 +1,10 @@
-
+import os
 import unittest
 
 import zopflipng
-test "can add":
+test "output file size less than original one":
   let src = "logo.png"
   let dest = "logo_out.png"
   optimizePNG(src, dest)
+  doAssert getFileSize(dest) > 0
+  doAssert getFileSize(dest) < getFileSize(src)
